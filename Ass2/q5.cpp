1b628fedfd3a0eb860802f5da70d7db1e0640b11
Vinay Sharma <vinaysharma1005@icloud.com>
2025-12-04 22:11:38 +0530

    done

---
+++ Ass2/q5.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout << "Enter size of diagonal matrix: ";
+    cin >> n;
+    int diag[n];   // only n elements
+    cout << "Enter diagonal elements:\n";
+    for (int i = 0; i < n; i++){
+        cin >> diag[i];
+    }
+    cout<<endl;
+    cout << "Matrix is:"<<endl;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i == j) cout << diag[i] << " ";
+            else cout << "0 ";
+        }
+        cout << endl;
+    }
+}
